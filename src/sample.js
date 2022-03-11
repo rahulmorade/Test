7b378e2a3123d931f7023fa6620cbc6163b201e7
unknown <rahulmorade11@gmail.com>
2022-03-11 23:52:36 +0530

    new commit 11/3/22

---
+++ src/sample.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function sample(){
+    return(
+        <div>
+            <h1>Sample component</h1>
+            <h2>new added line</h2>
+        </div>
+    )
+}
+export default sample;
